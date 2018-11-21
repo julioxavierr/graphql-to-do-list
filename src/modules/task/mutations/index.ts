db4cdd822ade17988af3ffc145eb4f4117d24473
Julio Xavier <julioxavierr@gmail.com>
2018-11-21 21:37:19 -0200

    feat(task): add mutations and update schema

---
+++ src/modules/task/mutations/index.ts
@@ -0,0 +1,9 @@
+import TaskAdd from './TaskAddMutation';
+import TaskCheck from './TaskCheckMutation';
+import TaskDelete from './TaskDeleteMutation';
+
+export default {
+  TaskAdd,
+  TaskCheck,
+  TaskDelete,
+};
--- src/type/MutationType.ts
+++ src/type/MutationType.ts
@@ -1,6 +1,9 @@
 import { GraphQLObjectType } from 'graphql';
+import TaskMutations from '../modules/task/mutations';
 
 export default new GraphQLObjectType({
   name: 'Mutation',
-  fields: () => ({}),
+  fields: () => ({
+    ...TaskMutations,
+  }),
 });
